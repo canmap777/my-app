de59862627b8c519f505b6ac06206509c7b76a9e
canmap777 <42286413+canmap777@users.noreply.github.com>
2020-11-15 20:59:14 -0500

    Update App.java

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,5 +9,12 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        System.out.println( " Welcome to Jenkins Demo!" );
+        System.out.println( "Pipeline!" );
+        System.out.println( "Multi-branch Pipeline" );
+        System.out.println( " ****************************************************************************************************" );
+        
+        
+        
     }
 }
